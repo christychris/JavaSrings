0a040d49c594321c0d173bc74fc6c25859025e8c
chris <chris@192.168.1.4>
2025-03-28 10:26:34 +0530

    Added newe class in Testclass1

---
+++ Test_Class1/src/TestPrograms/TestJavaHello.java
@@ -0,0 +1,10 @@
+package TestPrograms;
+
+public class TestJavaHello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world");
+	}
+
+}
